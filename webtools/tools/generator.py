6a6a21ece52350e771a42fdd1b43c3cc811b67ae
freewalk2333 <lj654864270@gmail.com>
2024-08-20 18:18:19 +0800

    loading...

--- webtools/tools/generator.py
+++ webtools/tools/generator.py
@@ -1,6 +1,6 @@
 import base64
 
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageOps
 from webtools.config.config import get_idcard_config
 import datetime
 import random
@@ -12,9 +12,10 @@ try:
 except:
     STXihei = r'C:/Windows/Fonts/simhei.ttf'
 
+
 def generate_idcard_frontside(name, idno, address):
-    idcard_source_path = os.path.join(os.path.dirname(__file__),'image/source/origin.jpg').replace('\\', '/')
-    idcard_save_path = os.path.join(os.path.dirname(__file__),'image/target/').replace('\\', '/')
+    idcard_source_path = os.path.join(os.path.dirname(__file__), 'image/source/portrait.jpg').replace('\\', '/')
+    idcard_save_path = os.path.join(os.path.dirname(__file__), 'image/target/').replace('\\', '/')
     image = Image.open(fp=idcard_source_path)
     draw = ImageDraw.Draw(im=image)
     name = list(name)
@@ -34,25 +35,25 @@ def generate_idcard_frontside(name, idno, address):
     addressfont = ImageFont.truetype(font=simhei, size=34)
     # 注入姓名：
     for i in range(len(name)):
-        draw.text(text=name[i], font=namefont, xy=(220+i*45, 101), fill=(0, 0, 0))
+        draw.text(text=name[i], font=namefont, xy=(220 + i * 45, 101), fill=(0, 0, 0))
     # 注入性别：
     draw.text(text=sex, font=sexfont, xy=(220, 179), fill=(0, 0, 0))
     # 注入出生年份：
     for i in range(len(year)):
-        draw.text(text=year[i], font=birthfont, xy=(220+i*15, 258), fill=(0, 0, 0))
+        draw.text(text=year[i], font=birthfont, xy=(220 + i * 15, 258), fill=(0, 0, 0))
     # 注入出生月份：
     for i in range(len(month)):
-        draw.text(text=month[i], font=birthfont, xy=(375+i*15, 258), fill=(0, 0, 0))
+        draw.text(text=month[i], font=birthfont, xy=(375 + i * 15, 258), fill=(0, 0, 0))
     # 注入出生日期：
     for i in range(len(day)):
-        draw.text(text=day[i], font=birthfont, xy=(463+i*15, 258), fill=(0, 0, 0))
+        draw.text(text=day[i], font=birthfont, xy=(463 + i * 15, 258), fill=(0, 0, 0))
     # 注入地址：
     for i in range(len(address)):
-        draw.text(text=address[i], font=addressfont, xy=(220+i*37, 338), fill=(0, 0, 0))
+        draw.text(text=address[i], font=addressfont, xy=(220 + i * 37, 338), fill=(0, 0, 0))
     # 注入身份证号：
     for i in range(len(id)):
-        draw.text(text=idno[i], font=idfont, xy=(373+i*30, 526), fill=(0, 0, 0))
-    filename = str(idno)+'_'+str(datetime.datetime.now().strftime('%Y%m%d%H%M%S' + '.png'))
+        draw.text(text=idno[i], font=idfont, xy=(373 + i * 30, 526), fill=(0, 0, 0))
+    filename = str(idno) + '_' + str(datetime.datetime.now().strftime('%Y%m%d%H%M%S' + '.png'))
     image.save(os.path.join(os.path.dirname(idcard_save_path), filename))
     with open(os.path.join(os.path.dirname(idcard_save_path), filename), 'rb') as file:
         image_base64_data = file.read()
@@ -61,14 +62,51 @@ def generate_idcard_frontside(name, idno, address):
 
 
 def generate_hehei():
-
     image = Image.new('RGBA', (511, 318), (0, 0, 0, 0))
     draw = ImageDraw.Draw(im=image)
     fontstyle = ImageFont.truetype(font=simhei, size=26)
     draw.text(text='图 片 在 此 展 示', font=fontstyle, xy=(145, 140), fill=(60, 60, 60))
     image.save('D:/AutomaticTest/IDcard/')
 
+
+def generate_idcard_backside(date_start, date_end):
+    fontstyle = ImageFont.truetype(font=simhei, size=55)
+    idcard_source_path = os.path.join(os.path.dirname(__file__), 'image/source/emblem.png').replace('\\', '/')
+    idcard_save_path = os.path.join(os.path.dirname(__file__), 'image/target/').replace('\\', '/')
+    image = Image.open(fp=idcard_source_path)
+    draw = ImageDraw.Draw(im=image)
+    date = list(str(date_start[0:4] + '.' + str(date_start[5:7]) + '.' + str(date_start[8:10]) + '-' + str(
+        date_end[0:4]) + '.' + str(date_end[5:7]) + '.' + str(date_end[8:10])))
+    date_start_year = list(str(date_start[0:4]) + '.')
+    date_start_month = list(str(date_start[5:7]) + '.')
+    date_start_day = list(str(date_start[8:10]))
+    date_end_year = list(str(date_end[0:4]))
+    date_end_month = list(str(date_end[5:7]))
+    date_end_day = list(str(date_end[8:10]))
+    x = 678
+    for i in range(len(date)):
+        if date[i].isdigit():
+            x += 13
+            draw.text(text=date[i], font=fontstyle, xy=(x, 835), fill=(0, 0, 0))
+            x += 14
+        elif date[i] == '-':
+            x += 16
+            draw.text(text=date[i], font=fontstyle, xy=(x, 841), fill=(0, 0, 0))
+            x += 16
+        else:
+            x += 13
+            draw.text(text=date[i], font=fontstyle, xy=(x, 835), fill=(0, 0, 0))
+            x += 2
+
+    filename = str(datetime.datetime.now().strftime('%Y%m%d%H%M%S' + '.png'))
+    image.save(os.path.join(os.path.dirname(idcard_save_path), filename))
+    with open(os.path.join(os.path.dirname(idcard_save_path), filename), 'rb') as file:
+        image_base64_data = file.read()
+        base64_str = base64.b64encode(image_base64_data).decode('utf-8')
+        return 'data:image/png;base64,' + base64_str
+
+
 if __name__ == '__main__':
     # print()
     # print(generate_idcard_frontside(name='张雷', idno='342623199810251234', address='北京市某个区某某某街道'))
-    generate_hehei()
+    generate_idcard_backside('2015-08-14', '2025-08-14')
--- webtools/views.py
+++ webtools/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from .tools.generator import generate_idcard_frontside
+from .tools.generator import generate_idcard_frontside, generate_idcard_backside
 import json
 
 @csrf_exempt
@@ -39,14 +39,12 @@ def emblem(request):
     if request.method == 'GET':
         return render(request=request, template_name='emblem.html')
     else:
-        name = request.POST['name']
-        idno = request.POST['idno']
-        address = request.POST['address']
-        idno_base64 = generate_idcard_frontside(name=name, idno=idno, address=address)
+        date_start = request.POST['date_start']
+        date_end = request.POST['date_end']
+        idno_base64= generate_idcard_backside(date_start=date_start, date_end=date_end)
         body = {
             'idno_base64': idno_base64,
         }
-
         return JsonResponse(body)
 
 @csrf_exempt
